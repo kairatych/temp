6b0a9cf93629d38c694c058ac87ac3b5d56f1d81
kairatych <doszhan.azimbayev@invogroup.com>
2022-07-31 18:57:18 +0600

    chore(): add form

--- test-task/src/app/app.component.ts
+++ test-task/src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UntypedFormControl, UntypedFormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test-task';
+export class AppComponent implements OnInit {
+  formGroup: UntypedFormGroup;
+  cadastralMask = '00-000-000-000';
+  phonePrefix = '+7';
+  phoneMask = '(000) 000-00-00';
+  numberErrorMessage: string;
+  cadastralNumberErrorMessage: string;
+  phoneNumberErrorMessage: string;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.formGroup = new UntypedFormGroup({
+      number: new UntypedFormControl(null, [
+        Validators.required,
+        Validators.pattern('/^-?(0|[1-9]\\d*)?$/'),
+        Validators.minLength(Number.MIN_SAFE_INTEGER),
+        Validators.maxLength(Number.MAX_SAFE_INTEGER),
+      ]),
+      cadastralNumber: new UntypedFormControl('', [
+        Validators.maxLength(255),
+      ]),
+      phoneNumber: new UntypedFormControl('', [
+        Validators.maxLength(255),
+      ]),
+    });
+  }
+
+  onSendForm() {
+    this.onCheck();
+  }
+
+  onCheck(): boolean {
+    let isValid = true;
+    if (!this.formGroup.get('number')?.value) {
+      this.numberErrorMessage = 'Введите номер';
+      this.messageTimeOut();
+      isValid = false;
+    } else if (!this.formGroup.get('cadastralNumber')?.value) {
+      this.cadastralNumberErrorMessage = 'Введите кадастровый номер';
+      this.messageTimeOut();
+      isValid = false;
+    } else if (this.formGroup.get('cadastralNumber')?.value.length < 11) {
+      this.cadastralNumberErrorMessage = 'Кадастровый номер введен не корректно';
+      this.messageTimeOut();
+      isValid = false;
+    } else if (!this.formGroup.get('phoneNumber')?.value) {
+      this.phoneNumberErrorMessage = 'Введите номер телефона';
+      this.messageTimeOut();
+      isValid = false;
+    } else if (this.formGroup.get('phoneNumber')?.value.length < 8) {
+      this.phoneNumberErrorMessage = 'Номер введен не корректно';
+      this.messageTimeOut();
+      isValid = false;
+    }
+    return isValid;
+  }
+
+  messageTimeOut(): void {
+    setTimeout(() => {
+      this.numberErrorMessage = '';
+      this.cadastralNumberErrorMessage = '';
+      this.phoneNumberErrorMessage = '';
+    }, 4000);
+  }
 }
--- test-task/src/app/app.module.ts
+++ test-task/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {NgxMaskModule} from "ngx-mask";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        NgxMaskModule.forRoot()
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
